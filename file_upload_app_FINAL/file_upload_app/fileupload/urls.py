b7147682a7a3cc8956e7b5a9b821625310000b78
SerahJoyceRasquinha <serahrasquinha@gmail.com>
2024-07-18 14:47:05 +0530

    changes

--- file_upload_app_FINAL/file_upload_app/fileupload/urls.py
+++ file_upload_app_FINAL/file_upload_app/fileupload/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('signout/', views.logout_view, name='logout'),
     path('upload/', views.upload_file, name='upload_file'),
     path('generate_report/', views.generate_report, name='generate_report'),
-    path('filtered_report/', views.filtered_report, name='filtered_report'),
     path('get_batch_names/', views.get_batch_names, name='get_batch_names'),
     path('get_subject_names/', views.get_subject_names, name='get_subject_names'),
+    path('report/', views.filtered_report, name='report'),
 ]
