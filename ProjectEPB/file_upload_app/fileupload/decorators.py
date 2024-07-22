5ceabd285f77cd984a286dbf7123f5c6e4144a65
SerahJoyceRasquinha <serahrasquinha@gmail.com>
2024-07-22 14:21:09 +0530

    The App

---
+++ ProjectEPB/file_upload_app/fileupload/decorators.py
@@ -0,0 +1,12 @@
+
+
+from django.shortcuts import redirect
+from functools import wraps
+
+def custom_login_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        return view_func(request, *args, **kwargs)
+    return wrapper
